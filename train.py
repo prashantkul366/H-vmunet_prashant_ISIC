614c25bb584da29f77313b0c1567fa2333095427
Prashant Kulkarni <prashantkul366@gmail.com>
2026-03-16 20:51:21 +0530

    hvmunet

--- train.py
+++ train.py
@@ -86,7 +86,7 @@ def main(config):
                                 drop_last=True)
     # test_dataset = isic_loader(path_Data = config.data_path, train = False, Test = True)
     test_dataset = Dataset(
-                        root=config.data_path, split="test",
+                        root=config.data_path, split="val",
                         images_dir="images", masks_dir="masks",
                         train_augs=False, target_size=(H, W)
                     )
